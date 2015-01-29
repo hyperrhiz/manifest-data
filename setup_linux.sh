cde9076817569983dbb732bdf35c69d277dec9ae
Luke Caldwell <luke@caldwell.pw>
2015-01-29 17:18:32 -0500

    Add linux setup script and fix Mac setup script

--- setup_linux.sh
+++ setup_linux.sh
@@ -9,4 +9,8 @@ DIR=$(dirname "$0")
 OUT=$DIR/.ids
 id -u > $OUT
 id -g >> $OUT
+
+## Install tcpflow and link it to /usr/local/bin
+sudo apt-get update && sudo apt-get install tcpflow
+sudo ln -s /usr/bin/tcpflow /usr/local/bin/tcpflow
 echo 'Setup complete!'
